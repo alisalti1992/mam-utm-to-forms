305d3db8878f3163d5f6be2c320539d1fe3e0068
Ali Sal <ali@moveaheadmedia.co.uk>
2021-10-01 11:00:19 +0700

    Added options page Updated the plugin name

--- mam-utm-to-forms.php
+++ mam-utm-to-forms.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Move Ahead Media UTMs To Forms
+ * Plugin Name: Move Ahead Media UTM To Forms
  * Plugin URI: https://github.com/moveaheadmedia/mam-utm-to-forms/
  * Description: This plugin helps users to get utm data into contact forms hidden fields!
  * Version: 1.0
@@ -8,3 +8,29 @@
  * Author URI: https://github.com/moveaheadmedia
  * Requires jQuery to be installed and ACF Pro plugin
  */
+
+function mam_utm_to_forms_admin_options() {
+
+    // Check function exists.
+    if( function_exists('acf_add_options_page') ) {
+        // parent page
+        acf_add_options_page(array(
+            'page_title' 	=> 'UTM To Forms',
+            'menu_title'	=> 'UTM To Forms',
+            'menu_slug' 	=> 'mam-utm-to-forms',
+            'capability'	=> 'read',
+            'redirect'		=> true
+        ));
+
+        // child page
+        acf_add_options_sub_page(array(
+            'page_title' 	=> 'UTM To Forms Options',
+            'menu_title'	=> 'UTM To Forms Options',
+            'menu_slug'  => 'mam-utm-to-forms-options',
+            'capability'	=> 'read',
+            'parent_slug'	=> 'mam-utm-to-forms'
+        ));
+    }
+}
+
+add_action('plugins_loaded', 'mam_utm_to_forms_admin_options');
