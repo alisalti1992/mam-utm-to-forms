45be018f6f6107fde9255f36fde15cc23efd25dc
Ali Sal <ali@moveaheadmedia.co.uk>
2021-10-01 10:20:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Move Ahead Media UTM To Form
+Download the plugin here: [here](https://github.com/moveaheadmedia/mam-utm-to-forms/releases)
+
+After the installation, Go to the plugin settings and configure the forms fields and that's it!
+
+Requires jQuery to be installed and ACF Pro plugin!
---
+++ mam-utm-to-forms.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Move Ahead Media UTM To Form
+ * Plugin URI: https://github.com/moveaheadmedia/mam-utm-to-forms/
+ * Description: This plugin helps users to get utm data into contact forms hidden fields!
+ * Version: 1.0
+ * Author: Move Ahead Media
+ * Author URI: https://github.com/moveaheadmedia
+ * Requires jQuery to be installed and ACF Pro plugin
+ */
